1e501db67d40f4523c993204102c497b8cee6b61
Miguel A. Muñoz <miguemunoz@openmailbox.org>
2017-05-02 21:41:26 +0100

    Solution for Project Euler #006 fixed. Now it passes all the tests (no timeouts).

---
+++ Project Euler #006 - Sum square difference/sum_square_difference.py
@@ -0,0 +1,14 @@
+import math
+import functools
+
+T = input()
+
+N = []
+
+for x in range(0,int(T)):
+    N.append(int(input()))
+
+for x in N:
+    s1 = (functools.reduce(lambda x, y: x+y, range(1,x+1))**2) 
+    s2 = ((x)*(x+1)*(2*x+1))//6
+    print(s1-s2)
