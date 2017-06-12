e78620708417a4a211ffdbdef34379c9acab6ccd
Miguel A. Munoz <miguemunoz@openmailbox.org>
2017-06-12 16:26:34 +0100

    The solution in Python was simplified (input parser removed).

--- Project Euler #003 - Largest prime factor/largest_prime_factor.py
+++ Project Euler #003 - Largest prime factor/largest_prime_factor.py
@@ -1,4 +1,3 @@
-
 def primes(n):
     primfac = []
     d = 2
@@ -11,39 +10,5 @@ def primes(n):
        primfac.append(n)
     return primfac
 
-def input_parser():
-    
-    input_values = []
-    
-    try:
-        T = int(input())
-    except ValueError:
-        print ('Invalid number.')
-        exit(-1)  
-        
-    if (T > 10 or T < 1):
-        print ('T value should be between 1 and 10.')
-        exit(-1)
-
-    for i in range(0,T):    
-        try:
-            input_values.append(int(input()))
-        except ValueError:
-            print ('Invalid number.')
-            exit(-1)
-        except EOFError:
-            print ('Invalid number of parameters.')
-            exit(-1)    
-            
-    for x in input_values:
-        if (x > 10**12 or x < 10):
-            print ('N value should be between 10 and 10^12.')
-            exit(-1)
-
-    return input_values
-            
-if __name__ == "__main__":
-    test_cases = input_parser()   
-    for x in test_cases:
-        v = int(x)
-        print(max(primes(v)))
+for x in range(0, int(input())):
+        print(max(primes(int(input()))))
