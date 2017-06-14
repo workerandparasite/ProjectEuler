0fa497db73e40f483198b1ecd4d54ea93e588991
Miguel A. Muñoz <miguemunoz@openmailbox.org>
2017-06-14 18:09:13 +0100

    Python code was simplified.

--- Project Euler #006 - Sum square difference/sum_square_difference.py
+++ Project Euler #006 - Sum square difference/sum_square_difference.py
@@ -1,12 +1,5 @@
-T = input()
-
-N = []
-
-for x in range(0,int(T)):
-    N.append(int(input()))
-
-for x in N:
+for x in range(int(input())):
+    x = int(input())
     s1 = sum(range(1,x+1))**2
     s2 = ((x)*(x+1)*(2*x+1))//6
     print(s1-s2)
-    
