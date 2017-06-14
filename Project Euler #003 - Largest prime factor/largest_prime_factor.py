991307a134c49e023f4acf066ca12977bd486aab
Miguel A. Munoz <miguemunoz@openmailbox.org>
2017-06-14 10:13:05 +0100

    Revert "Solution added in C++."
    This reverts commit 9b5c426056529817989359c65009bc7f452f3b65.

--- Project Euler #003 - Largest prime factor/largest_prime_factor.py
+++ Project Euler #003 - Largest prime factor/largest_prime_factor.py
@@ -6,10 +6,8 @@ def primes(n):
             primfac.append(d)  # supposing you want multiple factors repeated
             n //= d
         d += 1
-    print(primfac)
     if n > 1:
        primfac.append(n)
-    print(primfac)
     return primfac
 
 for x in range(0, int(input())):
